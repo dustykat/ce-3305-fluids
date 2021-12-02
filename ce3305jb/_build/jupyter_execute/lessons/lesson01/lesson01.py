1a1777af8ab6570ea320180ef5b1bdf086f20e8d
theodore cleveland <ted.cleveland@gmail.com>
2021-12-02 14:21:09 +0000

    update

---
+++ ce3305jb/_build/jupyter_execute/lessons/lesson01/lesson01.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+
+# 
+# ## Overview
+# The title of the course is Mechanics of Fluids.  The catalog description is: 
+# > CE 3305: Mechanics of Fluids (3:3:0).  Prerequisites: CE 2301.  Hydrostatics; dynamics of viscous and nonviscous fluids; resistance to flow; flow in pipes and open channels.
+# 
+# ## Purpose
+# The purpose of this required course is to provide engineering students with the fundamentals of fluid mechanics.  Students should be able to use this foundation for the more in-depth courses to follow.  This course shall provide students with a set of tools and concepts that are directly applicable to pipe systems, open channels, pumping plants, and measurement of fluid flows as well as other related problems that may be encountered as practicing engineers. 
+# 
+# ## Objectives:
+# Upon sucessful completion of this course, students will have demonstrated the Knowledge, Skill, and Ability (KSA) to:
+# 
+# 1. Identify and apply fluid properties to analyze and solve fluid mechanics problems
+# 2. Interpret conservation laws and apply them to analyze problems in hydrodynamics
+# 3. Identify and apply systems and control volume methods based on conservation principles
+# 4. Analyze steady pipe flow in networks using associated concepts and Computational Thinking (ENGR 1330) tools
+# 5. Differentiate and apply principles of dimensional homogeneity
+# 6. Analyze boundary layer flows and drag for hydraulic analysis
+# 7. Apply Computational Thinking (ENGR 1330) methods to analyze data and create mathematical models
+# 8. Recognize how specific sensors and instruments in fluid mechanics work
+# 9. Characterize, analyze and design open channels for steady one-dimensional flow
+# 
+# ## Assesment
+# 
+# The graded components of the course are:
+# 
+# - Quizzes (administered on a learning management system); individual activity
+# - Exercises (collected on a learning management system); individual activity
+# - Exams (administered on a learning management system); individual activity
+# 
+# The complete syllabus is located at [http://54.243.252.9/ce-3305-webroot/0-Syllabus/CE-3305-2022-1-syllabus.html](http://54.243.252.9/ce-3372-webroot/0-Syllabus/CE-3305-2022-1-syllabus.html)
+
+# ## Definition(s)
+# 
+# <hr>
+
+# ## Readings
+# 
+# CE-3305-2022-1 Syllabus. [http://54.243.252.9/ce-3305-webroot/0-Syllabus/CE-3305-2022-1-syllabus.html](http://54.243.252.9/ce-3372-webroot/0-Syllabus/CE-3305-2022-1-syllabus.html)
+# 
+# Engineering Fluid Mechanics (placeholder file to get links working). [http://54.243.252.9/ce-3305-webroot/3-Readings/EFM-1.pdf](http://54.243.252.9/ce-3372-webroot/3-Readings/EFM-1.pdf)
+# 
+
+# In[ ]:
+
+
+
+
