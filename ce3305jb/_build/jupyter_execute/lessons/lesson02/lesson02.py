1e129b7b27987d4161e5f1824c5d88fca6869fa6
theodore cleveland <ted.cleveland@gmail.com>
2021-12-30 21:01:03 +0000

    update jb lesson1

---
+++ ce3305jb/_build/jupyter_execute/lessons/lesson02/lesson02.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Intensive and Extensive Fluid Properties 
+# 
+# ## Lesson Outline
+# 1. Basic Fluid Properties
+#   - Intensive Properties
+#   - Extensive Properties
+# 2. Viscosimiters
+#   - Measuring viscosity
+#   - Example problems
+# 3. Vapor Pressure
+#   - Barometers
+
+# ## Readings
+# 
+# CE-3305-2022-1 Syllabus. [http://54.243.252.9/ce-3305-webroot/0-Syllabus/CE-3305-2022-1-syllabus.html](http://54.243.252.9/ce-3372-webroot/0-Syllabus/CE-3305-2022-1-syllabus.html)
+# 
+# Engineering Fluid Mechanics (placeholder file to get links working). [http://54.243.252.9/ce-3305-webroot/3-Readings/EFM-1.pdf](http://54.243.252.9/ce-3372-webroot/3-Readings/EFM-1.pdf)
+# 
+
+# In[ ]:
+
+
+
+
