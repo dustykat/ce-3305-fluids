2762ecedacfac53884feab83dc8c1fbe316a7dd1
theodore cleveland <ted.cleveland@gmail.com>
2022-05-19 20:21:02 +0000

    update jupyterbook

--- ce3305s22book/_build/jupyter_execute/lessons/lesson00/1problemsolving.py
+++ ce3305s22book/_build/jupyter_execute/lessons/lesson00/1problemsolving.py
@@ -3,10 +3,32 @@
 
 # # Problem Solving and Documentation
 # 
-# This is a section led by a level 1 header.
+# Three major work components for the course are:
 # 
-# :::{warning}
-# The header levels will interleave, so a careful proofing will be needed at some point, it wont destroy anything, but may misorder the author's intent
+# 1. Exercises (called "Exercise Sets")
+# 2. Quizzes
+# 3. Exams
+# 
+# :::{warning} 
+# All the above instruments should be engaged with a mindset of engaging in the practice of problem solving as a process (regardless of how difficult the actual problem may be).  Most of the problems are  [Cheggable](https://www.chegg.com/); however the Chegg solutions are pretty minimal and often incorrect (Input values change, and the tutors dont fucking read the problems).  Use Chegg if you wish with this limitation in mind - beware in other classes Chegg is considered cheating; You are warned!
 # :::
 # 
-# weird warning at compile
+# The exercises and exams (and to a lesser extent the quizzes) purpose is threefold: 
+# 
+# 1. Practice a systematic method to solve and document closed engineering problems;
+# 2. Develop a type of "muscle memory" in applying the process;
+# 3. Become accustomed to documenting effort so that others can replicate your work.
+# 
+# The fundamental protocol is
+# 
+# 1. State the problem
+# 2. Sketch the situation
+# 3. List known quantities
+# 4. List unknown quantities
+# 5. Identify and list governing principles, assumptions, and equations
+# 6. Analyse the problem - solve for the unknowns
+# 7. Discuss the results; validate when possible, examine effect of changing an assumption.
+# 
+# There is also an issue of format or the visual structure of a solution.  The use of Jupyter Notebooks as your computation tool is expected - as part of a documentation process and the WCOE's integration of computational thinking throughout the curriculum.  Most problems you will work a large part by hand then transfer the computational burden the the Jupyter Notebook. 
+# 
+# 
