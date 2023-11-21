51c259efd02c361b04bc6123811eb70ad87dcf1a
theodore cleveland <ted.cleveland@gmail.com>
2023-11-21 20:32:56 +0000

    notes

---
+++ ce3305s24book/_build/jupyter_execute/lessons/02fluidproperties/6exercise.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exercise Set 1
+# 
+# :::{Note}
+# The preferred way to access is through the Blackboard Learning Management System; its where you upload your solution.
+# :::
+# 
+# [http://54.243.252.9/ce-3305-webroot/2-Exercises/ES1/ES1.html](http://54.243.252.9/ce-3305-webroot/2-Exercises/ES1/ES1.html)
+
+# In[ ]:
+
+
+
+
