6c281724f1cc9d10fa22e39b7d57b47ac4369577
theodore cleveland <ted.cleveland@gmail.com>
2023-11-21 18:53:11 +0000

    update book links

--- ce3305s24book/_build/jupyter_execute/lessons/21chydpipes/1chydpipes.py
+++ ce3305s24book/_build/jupyter_execute/lessons/21chydpipes/1chydpipes.py
@@ -2396,32 +2396,7 @@ if verbose == 'true' :
 # ipsum lorem
 # 
 
-# ### Topic 2
-# 
-# ipsum lorem
-# 
-# #### Subtopic 
-# 
-# ipsum lorem
-# 
-# #### Subtopic
-# 
-# ipsum lorem
-# 
-
-# ### Topic 3
-# 
-# ipsum lorem
-# 
-# #### Subtopic 
-# 
-# ipsum lorem
-# 
-# #### Subtopic
-# 
-# ipsum lorem
-# 
-
+# <!--
 # ## Readings
 # 
 # 1. [CE-3305-2022-2 (Summer2) Syllabus](http://54.243.252.9/ce-3305-webroot-su22/0-Syllabus/ce-3305-2022-1-syllabus.html)
@@ -2431,6 +2406,7 @@ if verbose == 'true' :
 # 3. DF Elger, BC Williams, Crowe, CT and JA Roberson, *Engineering Fluid Mechanics 10th edition*, John Wiley & Sons, Inc., 2013.  (placeholder file to get links working). [http://54.243.252.9/ce-3305-webroot/3-Readings/EFM-1.pdf](http://54.243.252.9/ce-3305-webroot/3-Readings/EFM-1.pdf)
 # 
 # 4. Cleveland, T. G. (2014) *Fluid Mechanics Notes to Accompany CE 3305 at Jade-Holshule (TTU Study Abroad 2015-2019)*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering. [http://54.243.252.9/ce-3305-webroot/3-Readings/ce3305-lecture-001.1.pdf](http://54.243.252.9/ce-3305-webroot/3-Readings/ce3305-lecture-001.1.pdf)
+# -->
 
 # ## Exercises
 # 
